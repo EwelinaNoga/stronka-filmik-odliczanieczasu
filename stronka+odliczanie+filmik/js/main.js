603fa1a3ad9b85af2340d7f5e569f1c622170202
Ewelina Noga <ewlina.noga@gmail.com>
2018-05-02 13:36:19 +0200

    bez filmiku

---
+++ stronka+odliczanie+filmik/js/main.js
@@ -0,0 +1,100 @@
+ // Set the date we're counting down to
+ var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+ // Update the count down every 1 second
+ var x = setInterval(function () {
+     // Get todays date and time
+     var now = new Date().getTime();
+     // Find the distance between now an the count down date
+     var distance = countDownDate - now;
+     // Time calculations for days, hours, minutes and seconds
+     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     // Display the result in the element with id="demo"
+     document.getElementById("days").innerHTML = days + "d ";
+     document.getElementById("hours").innerHTML = hours + "h ";
+     document.getElementById("minutes").innerHTML = minutes + "m ";
+     document.getElementById("secundes").innerHTML = seconds + "s ";
+     // If the count down is finished, write some text 
+     if (distance < 0) {
+         clearInterval(x);
+         document.getElementById("days").innerHTML = "EXPIRED";
+         document.getElementById("hours").innerHTML = "EXPIRED";
+         document.getElementById("minutes").innerHTML = "EXPIRED";
+         document.getElementById("secundes").innerHTML = "EXPIRED";
+     }
+ }, 1000);
+
+
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
+
+var v = document.getElementsByTagName('video')[0];
+      
+      v.addEventListener('timeupdate', function()
+      {
+        var p = document.getElementsByTagName('progress')[0];
+        p.value = 100*this.currentTime/this.duration;
+      });
+
